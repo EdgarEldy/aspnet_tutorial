6f87150b64912ff16df16b66138124f80ed63ef4
Edgar Eldy <ed.eldy21@live.com>
2022-01-06 22:54:23 +0200

    Add ApplicationDbContext class for handling models

---
+++ aspnet_tutorial/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace aspnet_tutorial.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Constructor
+        public ApplicationDbContext()
+        {
+
+        }
+    }
+}
