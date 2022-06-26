7c6f3cd1b178cfd2a6c2c2fb78b0cb8424c7d68d
Edgar Eldy <ed.eldy21@live.com>
2022-06-26 15:05:05 +0200

    Register Models

--- aspnet_tutorial/Data/ApplicationDbContext.cs
+++ aspnet_tutorial/Data/ApplicationDbContext.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
+using aspnet_tutorial.Models;
 
 namespace aspnet_tutorial.Data
 {
@@ -14,5 +11,14 @@ namespace aspnet_tutorial.Data
         {
 
         }
+        
+        // Register models here..
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
     }
 }
