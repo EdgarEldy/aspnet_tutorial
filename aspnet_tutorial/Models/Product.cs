78efd33526ea2f1bf1b5dde8b3282d7d86e73a38
Edgar Eldy <ed.eldy21@live.com>
2022-06-26 14:11:45 +0200

    Add relationship to Category Model

--- aspnet_tutorial/Models/Product.cs
+++ aspnet_tutorial/Models/Product.cs
@@ -15,6 +15,7 @@ namespace aspnet_tutorial.Models
         // Properties goes here..
         public int Id { get; set; }
 
+        [ForeignKey("CategoryId")]
         public int? CategoryId { get; set; }
 
         public string ProductName { get; set; }
@@ -24,5 +25,8 @@ namespace aspnet_tutorial.Models
         public DateTime? CreatedAt { get; set; }
         
         public DateTime? UpdatedAt { get; set; }
+
+        // Add relationship to Category Model
+        public virtual Category Category { get; set; }
     }
 }
