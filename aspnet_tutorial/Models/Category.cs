17116e189fbd820dafe5a72ca6654ff368778cb2
Edgar Eldy <ed.eldy21@live.com>
2022-06-26 14:02:32 +0200

    Add Category Model

---
+++ aspnet_tutorial/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace aspnet_tutorial.Models
+{
+    [Table("Categories")]
+    public class Category
+    {
+        // Constructor
+        public Category()
+        {
+            
+        }
+    }
+}
