34c7ad8fb9bfdf4c3c1664ce4ebda6895f367e24
Edgar Eldy <ed.eldy21@live.com>
2022-06-26 15:02:44 +0200

    Add relationship to Product Model

--- aspnet_tutorial/Models/Order.cs
+++ aspnet_tutorial/Models/Order.cs
@@ -20,6 +20,7 @@ namespace aspnet_tutorial.Models
         [ForeignKey("CustomerId")]
         public int? CustomerId { get; set; }
 
+        [ForeignKey("ProductId")]
         public int? ProductId { get; set; }
 
         public double Qty { get; set; }
@@ -32,5 +33,8 @@ namespace aspnet_tutorial.Models
 
         // Add relationship to Customer Model
         public virtual Customer Customer { get; set; }
+        
+        // Add relationship to Product Model
+        public virtual Product Product { get; set; }
     }
 }
