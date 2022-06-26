a32ce4fb5fac2eea092bf5bb54ca8dbe295a8392
Edgar Eldy <ed.eldy21@live.com>
2022-06-26 14:56:37 +0200

    Update Customer Model : Add properties

--- aspnet_tutorial/Models/Customer.cs
+++ aspnet_tutorial/Models/Customer.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace aspnet_tutorial.Models
 {
@@ -10,5 +12,23 @@ namespace aspnet_tutorial.Models
         {
             
         }
+        
+        // Properties goes here
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Tel { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+        
+        public DateTime? CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
     }
 }
