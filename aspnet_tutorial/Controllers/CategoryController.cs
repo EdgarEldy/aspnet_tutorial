e339f6bc4cbffa1c36d352ed9e722dc05ab44e8d
Edgar Eldy <ed.eldy21@live.com>
2022-06-29 21:47:39 +0200

    Add CategoryController

---
+++ aspnet_tutorial/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace aspnet_tutorial.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
