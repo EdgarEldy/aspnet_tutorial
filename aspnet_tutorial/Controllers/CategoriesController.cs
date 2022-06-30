80753338a560e3721a9e04c604cae8f3d51cfe5a
Edgar Eldy <ed.eldy21@live.com>
2022-06-30 21:51:08 +0200

    Add CategoriesController and add Index action

---
+++ aspnet_tutorial/Controllers/CategoriesController.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using aspnet_tutorial.Data;
+
+namespace aspnet_tutorial.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+        // GET
+        public async Task<ActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+    }
+}
