63f2e804fa7a9428dfa05e63a164e9a02a9da96d
Edgar Eldy <ed.eldy21@live.com>
2022-07-02 11:44:29 +0200

    Save A new category

--- aspnet_tutorial/Controllers/CategoriesController.cs
+++ aspnet_tutorial/Controllers/CategoriesController.cs
@@ -1,23 +1,36 @@
 ﻿using System.Data.Entity;
 using System.Threading.Tasks;
 using System.Web.Mvc;
-using aspnet_tutorial.Data;
+using aspnet_tutorial.Models;
+using ApplicationDbContext = aspnet_tutorial.Data.ApplicationDbContext;
 
 namespace aspnet_tutorial.Controllers
 {
     public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _context = new ApplicationDbContext();
+
         // GET
         public async Task<ActionResult> Index()
         {
             return View(await _context.Categories.ToListAsync());
         }
-        
+
         // GET: Categories/Create
         public ActionResult Create()
         {
             return View();
+        }
+
+        // POST: Categories/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id, CategoryName")] Category category)
+        {
+            if (!ModelState.IsValid) return View(category);
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
