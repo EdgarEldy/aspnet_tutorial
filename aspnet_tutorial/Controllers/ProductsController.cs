efda22e818ad186fa31af3d20eeb402b204d7767
Edgar Eldy <ed.eldy21@live.com>
2022-07-06 07:58:54 +0200

    Initialize DbContexts

--- aspnet_tutorial/Controllers/ProductsController.cs
+++ aspnet_tutorial/Controllers/ProductsController.cs
@@ -1,9 +1,12 @@
 ﻿using System.Web.Mvc;
+using aspnet_tutorial.Data;
 
 namespace aspnet_tutorial.Controllers
 {
     public class ProductsController : Controller
     {
+        // Initialize DbContexts
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
         // GET
         public ActionResult Index()
         {
