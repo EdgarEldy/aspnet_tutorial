e20ae5f95b5f7068e25f8c8ce765d8e20c131ece
Edgar Eldy <ed.eldy21@live.com>
2022-07-13 11:04:53 +0200

    Get Customers List

--- aspnet_tutorial/Controllers/CustomersController.cs
+++ aspnet_tutorial/Controllers/CustomersController.cs
@@ -1,13 +1,18 @@
-﻿using System.Web.Mvc;
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using aspnet_tutorial.Data;
 
 namespace aspnet_tutorial.Controllers
 {
     public class CustomersController : Controller
     {
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
         // GET: Customers
-        public ActionResult Index()
+        public async  Task<ActionResult> Index()
         {
-            return View();
+            var customers = await _context.Customers.ToListAsync();
+            return View(customers);
         }
     }
 }
