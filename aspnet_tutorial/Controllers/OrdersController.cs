72548ff4599f4ff190acbf8e496520f1ad690648
Edgar Eldy <ed.eldy21@live.com>
2022-07-15 20:46:23 +0200

    Add OrdersController

---
+++ aspnet_tutorial/Controllers/OrdersController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using aspnet_tutorial.Data;
+
+namespace aspnet_tutorial.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
