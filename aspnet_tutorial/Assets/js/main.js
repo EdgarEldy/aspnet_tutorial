1803bc51926ffd378622388a972c1018b9012b41
EdgarEldy <ed.eldy21@live.com>
2022-07-21 12:52:27 +0200

    Fixed ids

--- aspnet_tutorial/Assets/js/main.js
+++ aspnet_tutorial/Assets/js/main.js
@@ -1,11 +1,12 @@
-﻿$(function () {
-    $('#cat_id').on('change', function () {
+﻿// Get products by category id
+$(function () {
+    $('#CategoryId').on('change', function () {
         var id = $(this).val();
         //alert(id);
-        $.get('/Orders/getProducts', { id: id }, function (data) {
-            $('#product_id').empty();
+        $.get('/Orders/GetProducts', { id: id }, function (data) {
+            $('#ProductId').empty();
             $.each(data, function (index, row) {
-                $('#product_id').append("<option value='" + row.id + "'>" + row.product_name + "</option>")
+                $('#ProductId').append("<option value='" + row.Id + "'>" + row.ProductName + "</option>")
             });
             });
     });
