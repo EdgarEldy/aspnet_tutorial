e72e12764421a98eddc0537a945b87418e907bcd
Edgar Eldy <ed.eldy21@live.com>
2022-08-10 20:38:53 +0200

    Add User properties

--- aspnet_tutorial/Models/User.cs
+++ aspnet_tutorial/Models/User.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace aspnet_tutorial.Models
 {
@@ -8,7 +10,38 @@ namespace aspnet_tutorial.Models
         // Constructor
         public User()
         {
-            
         }
+
+        // Properties
+        [Key] public long Id { get; set; }
+        [Required] 
+        [StringLength(100)] 
+        public string FirstName { get; set; }
+        
+        [Required] 
+        [StringLength(100)] 
+        public string LastName { get; set; }
+        
+        public string Email { get; set; }
+        
+        public bool? EmailConfirmed { get; set; }
+        
+        public string PasswordHash { get; set; }
+        
+        public string SecurityStamp { get; set; }
+        
+        public string PhoneNumber { get; set; }
+        
+        public bool? PhoneNumberConfirmed { get; set; }
+        
+        public bool? TwoFactorEnabled { get; set; }
+        
+        public DateTime? LockoutEndDateUtc { get; set; }
+        
+        public bool? LockoutEnabled { get; set; }
+        
+        public int? AccessFailedCount { get; set; }
+        
+        public string UserName { get; set; }
     }
 }
