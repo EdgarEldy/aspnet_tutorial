e6005cc77ac9c49db46b9bc65b2ea859f812e069
Edgar Eldy <ed.eldy21@live.com>
2022-08-10 20:40:06 +0200

    Add UserClaim properties

--- aspnet_tutorial/Models/UserClaim.cs
+++ aspnet_tutorial/Models/UserClaim.cs
@@ -1,14 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace aspnet_tutorial.Models
 {
     [Table("users_claims")]
     public class UserClaim
     {
-     // Constructor
-     public UserClaim()
-     {
-         
-     }
+        // Constructor
+        public UserClaim()
+        {
+        }
+        
+        // Properties
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("User")]
+        public long UserId { get; set; }
+
+        public string ClaimType { get; set; }
+
+        public string ClaimValue { get; set; }
     }
 }
