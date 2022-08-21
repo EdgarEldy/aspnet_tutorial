dd19e58c07e655cc50dfbe1046d1966e2b3da3b2
Edgar Eldy <ed.eldy21@live.com>
2022-08-21 22:02:28 +0200

    Add AuthSeeder class for handling authentication seeders (roles, permissions and users)

---
+++ aspnet_tutorial/Seeders/AuthSeeder.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using aspnet_tutorial.Data;
+
+namespace aspnet_tutorial.Seeders
+{
+    public class AuthSeeder: CreateDatabaseIfNotExists<ApplicationDbContext>
+    {
+        
+    }
+}
