115dddc694f7e39746680485546e036d460b99e0
Edgar Eldy <ed.eldy21@live.com>
2022-08-21 22:17:03 +0200

    Add permissions seeder

--- aspnet_tutorial/Seeders/AuthSeeder.cs
+++ aspnet_tutorial/Seeders/AuthSeeder.cs
@@ -1,10 +1,65 @@
-﻿using System.Data.Entity;
-using aspnet_tutorial.Data;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using aspnet_tutorial.Models;
+using ApplicationDbContext = aspnet_tutorial.Data.ApplicationDbContext;
 
 namespace aspnet_tutorial.Seeders
 {
-    public class AuthSeeder: CreateDatabaseIfNotExists<ApplicationDbContext>
+    public class AuthSeeder : CreateDatabaseIfNotExists<ApplicationDbContext>
     {
-        
+        // Permissions seeder
+        public static void PermissionSeeder(ApplicationDbContext context)
+        {
+            if (context.Permissions.Any()) return;
+            var permissions = new List<string>
+            {
+                // Roles
+                "roles.create",
+                "roles.show",
+                "roles.edit",
+                "roles.delete",
+
+                // Users
+                "users.create",
+                "users.show",
+                "users.edit",
+                "users.delete",
+
+                // Categories
+                "categories.create",
+                "categories.show",
+                "categories.edit",
+                "categories.delete",
+
+                // Products
+                "products.create",
+                "products.show",
+                "products.edit",
+                "products.delete",
+
+                // Customers
+                "customers.create",
+                "customers.show",
+                "customers.edit",
+                "customers.delete",
+
+                // Orders
+                "orders.create",
+                "orders.show",
+                "orders.edit",
+                "orders.delete",
+            };
+
+            foreach (var permission in permissions)
+            {
+                context.Permissions.Add(new Permission
+                {
+                    Name = permission
+                });
+            }
+
+            context.SaveChanges();
+        }
     }
 }
