b3437a1c5297f7313e0e0300161759a7db186119
EdgarEldy <ed.eldy21@live.com>
2022-09-19 10:04:24 +0200

    Update UserRole: Add UserId and RoleId foreign keys

--- aspnet_tutorial/Models/UserRole.cs
+++ aspnet_tutorial/Models/UserRole.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace aspnet_tutorial.Models
 {
@@ -10,5 +11,16 @@ namespace aspnet_tutorial.Models
         {
             
         }
+
+        // Properties
+        [Key]
+        [Column(Order = 1)]
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        [ForeignKey("Role")]
+        public int RoleId { get; set; }
     }
 }
