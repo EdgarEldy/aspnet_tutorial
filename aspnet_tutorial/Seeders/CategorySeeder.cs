10139fd4d02e0cd524a69e23bc95d6b351a06716
EdgarEldy <ed.eldy21@live.com>
2022-09-26 11:41:16 +0200

    Add CategorySeeder class

---
+++ aspnet_tutorial/Seeders/CategorySeeder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using aspnet_tutorial.Data;
+
+namespace aspnet_tutorial.Seeders
+{
+    public class CategorySeeder : CreateDatabaseIfNotExists<ApplicationDbContext>
+    {
+    }
+}
