902a09d84614753d1e0fb2302b351beed193ff00
EdgarEldy <ed.eldy21@live.com>
2022-09-26 11:54:01 +0200

    Add Category Seeders

--- aspnet_tutorial/Seeders/CategorySeeder.cs
+++ aspnet_tutorial/Seeders/CategorySeeder.cs
@@ -3,11 +3,39 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
-using aspnet_tutorial.Data;
+using aspnet_tutorial.Models;
+using ApplicationDbContext = aspnet_tutorial.Data.ApplicationDbContext;
 
 namespace aspnet_tutorial.Seeders
 {
     public class CategorySeeder : CreateDatabaseIfNotExists<ApplicationDbContext>
     {
+        // Category Seeders
+        public static void Run(ApplicationDbContext context)
+        {
+            if (context.Categories.Any()) return;
+
+            var categories = new List<Category>()
+            {
+                new Category()
+                {
+                    CategoryName = "Lemonades",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now
+                },
+                new Category()
+                {
+                    CategoryName = "Alcohols",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now
+                },
+            };
+
+            foreach (var category in categories)
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
     }
 }
