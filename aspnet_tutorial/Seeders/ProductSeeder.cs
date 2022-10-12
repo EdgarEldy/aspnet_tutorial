1c076bbf6a96a6ea0200a880c68202b4998c1f50
EdgarEldy <ed.eldy21@live.com>
2022-10-12 14:38:12 +0200

    Add ProductSeeder class

---
+++ aspnet_tutorial/Seeders/ProductSeeder.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+using aspnet_tutorial.Data;
+
+namespace aspnet_tutorial.Seeders
+{
+    public class ProductSeeder : CreateDatabaseIfNotExists<ApplicationDbContext>
+    {
+        // Products data seeder goes here..
+        
+    }
+}
