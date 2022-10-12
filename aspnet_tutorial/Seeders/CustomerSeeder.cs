91d716bd224bb932a1cc813a3944d0a9a4c34193
EdgarEldy <ed.eldy21@live.com>
2022-10-12 14:41:32 +0200

    Add CustomerSeeder class

---
+++ aspnet_tutorial/Seeders/CustomerSeeder.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using aspnet_tutorial.Data;
+
+namespace aspnet_tutorial.Seeders
+{
+    public class CustomerSeeder : CreateDatabaseIfNotExists<ApplicationDbContext>
+    {
+        // Customers data seeder goes here..
+    }
+}
